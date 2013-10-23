1803cdf07fc9def64205f23c8d3f6ace741b3d6b
Your Name <your.email@example.com>
2013-10-23 13:25:40 +0700

    update

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -9,6 +9,7 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @teams = @user.joined_teams.paginate(page: params[:page], per_page: 3)
   end
 
   def new
